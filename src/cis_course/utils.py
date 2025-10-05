9bba0f0708439fb1ae88075b46fe78a6adf13bcd
Hans Smit <hans.smit@ext.esa.int>
2025-10-05 18:20:29 +0000

    Refactored utils.py module with tests.

--- src/cis_course/utils.py
+++ src/cis_course/utils.py
@@ -1,35 +1,70 @@
 from pathlib import Path
 import numpy as np
 
+from dataclasses import dataclass
 
-def bytes_to_uint16(byte_seq: bytes, index: int) -> int:
-    return int.from_bytes(byte_seq[index:index + 2], byteorder="little")
 
-    
-def extract_images(image_file: Path | str) -> list[np.ndarray]:
+@dataclass
+class Header:
+    """Image header data structure."""
+
+    sz: int
+    res1: int
+    res2: int
+    rows: int
+    cols: int
+    bits: int
+
+    def __post_init__(self):
+        assert self.sz == 6
+
+    @property
+    def image_size(self) -> int:
+        """Return the image (data) size in bytes."""
+        return self.rows * self.cols * (self.bits // 8)
+
+    @property
+    def dtype(self) -> str:
+        """Return the numpy data-type."""
+        # TODO: how to determine unsigned int or float types?
+        if self.bits == 16:
+            return "int16"
+        if self.bits == 32:
+            return "int32"
+        if self.bits == 64:
+            return "int64"
+        raise ValueError(f"Cannot determine data-type. {self.bits=}")
+
+
+def parse_header(byte_seq: bytes, index: int = 0) -> Header:
+    """Extract the (rows, cols) from the header."""
+    # typical byte_seq:
+    # b'\x06\x00\x00\x00\x00\x00*\x01,\x01\x10\x00'
+    #   | size  | ???   |???    |rows|cols|bits   |
+    #  [6,      0,      0,      298, 300, 16]
+    # i:0       1       2       3    4    5
+    nums = np.frombuffer(byte_seq, dtype=np.uint16, count=6, offset=index)
+    nums_int = [int(v) for v in nums]  # convert from `numpy.uint16` list to `int` list
+    header = Header(*nums_int)
+    return header
+
+
+def load_images(image_file: Path | str) -> list[np.ndarray]:
     """Parse the binary image file and return a list of image arrays."""
+    # TODO: implement this as generator (with yield)
     result = []
+
     pth = Path(image_file).expanduser()
     with open(pth, mode="rb") as f:
-        img_all_bytes = f.read()
+        header_bytes = f.read(12)
 
-    header_size = bytes_to_uint16(img_all_bytes, 0)
-    assert header_size == 6
-    header = [bytes_to_uint16(img_all_bytes, i) for i in range(0, header_size * 2, 2)]
-    
-    rows, cols = header[3:5]
-    hdr_byte_sz = header_size * 2
-    img_byte_sz = (rows * cols) * 2  # 16 bit numbers
-    tot_byte_sz = hdr_byte_sz + img_byte_sz
-    i = 0
-    while True:
-        data = img_all_bytes[tot_byte_sz * i: tot_byte_sz * (i + 1)]
-        if not data:
-            break
-        img_bytes = data[12:]
-        assert data[0] == 0x06
-        img = np.frombuffer(img_bytes, dtype="int16")
-        img = img.reshape((rows, cols))
-        result.append(img)
-        i += 1
+        # Loop over all data bytes (header + image) until the end is reached.
+        while header_bytes:
+            header = parse_header(header_bytes, 0)
+            image_bytes = f.read(header.image_size)
+            image = np.frombuffer(image_bytes, dtype=header.dtype)
+            image = image.reshape((header.rows, header.cols))
+            result.append(image)
+            header_bytes = f.read(12)  # get next header
+
     return result
